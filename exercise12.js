d82e814b1cfe00d2761703a0d5139b7aa7f91d6d
Demi Lee <demilee.to@gmail.com>
2017-11-14 16:20:30 -0500

    exercise12

---
+++ exercise12.js
@@ -0,0 +1,5 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+console.log("Todo:");
+items.forEach(function(item, i) {
+  console.log((i+1) + ": " + item);
+});
